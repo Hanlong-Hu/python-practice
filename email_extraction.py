817322abe126cd9fdb6c1bb8736cca8c44ee7998
Hanlong Hu <z5477747@ad.unsw.edu.au>
2025-11-11 16:15:05 +1100

    utility functions

--- email_extraction.py
+++ email_extraction.py
@@ -1,1 +1,11 @@
+import re
+
+def extract_email_list(file = "/home/hanlong/python-practice/data/email_exchanges_big.txt"):
+    with open(file, 'r') as f:
+        text = f.read()
+        emails = re.findall(r"[a-zA-Z0-9_.±]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+", text)
+        emails_set = set(emails)
+        return emails_set
+
+print(extract_email_list())
 
--- word_counter.py
+++ word_counter.py
@@ -1,10 +1,14 @@
 from collections import Counter
+import re
 
-def find_most_common_words(file= "/home/hanlong/python-practice/data/data.txt", number=10):
+def find_most_common_words(file= "/home/hanlong/python-practice/data/donald_speech.txt", number=10):
     with open(file, 'r') as f:
-        lines = f.readlines()
-        words = [w for line in lines for w in line.split().lower()]
+        text = f.read().lower()
+        words = re.findall(r"[a-zA-Z0-9']+", text)
         counter = Counter(words)
         return counter.most_common(number)
+
+print(find_most_common_words())
+
 
 
